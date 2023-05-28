fe1d0fb38711f67d45f1cf65bcb0d206ee33e6fb
Zuzanna <zuzaszewczyk3@gmail.com>
2023-05-28 19:36:59 +0200

    prototypegui

--- src/main/java/Project/App.java
+++ src/main/java/Project/App.java
@@ -1,10 +1,15 @@
 package Project;
+import Project.TasksList;
+
 
 import javafx.application.Application;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 import java.io.IOException;
 
@@ -14,10 +19,30 @@ import java.io.IOException;
 public class App extends Application {
 
     private static Scene scene;
+    public static TasksList tasksList;
+    public static int sizeOfTasks;
+
+    public static CurrentTask currentTask;
+
+    private double xOffset = 0;
+    private double yOffset = 0;
+
+    private static Parent root;
 
     @Override
     public void start(Stage stage) throws IOException {
-        scene = new Scene(loadFXML("task"), 757, 535);
+        scene = new Scene(loadFXML("ViewText"), 707, 435);
+        stage.initStyle(StageStyle.UNDECORATED);
+
+        root.setOnMousePressed(event -> {
+            xOffset = event.getSceneX();
+            yOffset = event.getSceneY();
+        });
+        root.setOnMouseDragged(event -> {
+            stage.setX(event.getScreenX() - xOffset);
+            stage.setY(event.getScreenY() - yOffset);
+        });
+
         stage.setScene(scene);
         stage.show();
     }
@@ -28,10 +53,16 @@ public class App extends Application {
 
     private static Parent loadFXML(String fxml) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource(fxml + ".fxml"));
-        return fxmlLoader.load();
+        root = fxmlLoader.load();
+        return root;
     }
 
     public static void main(String[] args) {
+        tasksList = new TasksList();
+        tasksList.takeNamesOfFiles();
+        sizeOfTasks = tasksList.listOfTasks.size();
+        currentTask = new CurrentTask();
+        //TO DO ustaw staly pierwszy task wysiwetlany
         launch();
     }
 
---
+++ src/main/java/Project/Pair.java
@@ -0,0 +1,32 @@
+package Project;
+
+public class Pair<T, U> {
+    private T first;
+    private U second; //second member of pair
+
+    public Pair(T first, U second) {
+        this.first = first;
+        this.second = second;
+    }
+
+    public void setFirst(T first) {
+        this.first = first;
+    }
+
+    public void setSecond(U second) {
+        this.second = second;
+    }
+
+    public void setBoth(T first, U second){
+        this.first = first;
+        this.second = second;
+    }
+
+    public T getFirst() {
+        return first;
+    }
+
+    public U getSecond() {
+        return second;
+    }
+}
