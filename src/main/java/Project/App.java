12ff05b7e18d1fcaaa5414bb08508f802c20a677
Antoni Długosz <antonidlugosz@Antonis-MacBook-Air.local>
2023-06-10 17:29:52 +0200

    added loading screen

--- src/main/java/Project/App.java
+++ src/main/java/Project/App.java
@@ -2,20 +2,28 @@ package Project;
 
 import Project.Controllers.CodeEditorController;
 import Project.Controllers.ShowTaskController;
+import javafx.animation.FadeTransition;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Cursor;
+import javafx.scene.Group;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.control.TextArea;
+import javafx.scene.effect.BlendMode;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.HBox;
+import javafx.scene.paint.ImagePattern;
+import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
+import javafx.util.Duration;
 
 import java.io.IOException;
 import java.util.List;
@@ -50,6 +58,41 @@ public class App extends Application {
             stage.setX(event.getScreenX() - xOffset);
             stage.setY(event.getScreenY() - yOffset);
         });
+        stage.setScene(scene);
+        Image img = new Image(App.class.getResource("loadingScreen.jpg").toExternalForm());
+        ImageView imgView = new ImageView(img);
+        ImageView imgView2 = new ImageView(img);
+        imgView.setBlendMode(BlendMode.MULTIPLY);
+        imgView.setFitWidth(1200);
+        imgView.setFitHeight(800);
+        imgView2.setFitWidth(1200);
+        imgView2.setFitHeight(800);
+        Group loadingRoot = new Group(imgView2, imgView);
+        scene.setRoot(loadingRoot);
+        FadeTransition ft = new FadeTransition(Duration.millis(3000), imgView);
+        ft.setFromValue(1.0);
+        ft.setToValue(0.0);
+        ft.setCycleCount(1000 * 1000);
+        ft.setAutoReverse(true);
+        ft.play();
+        scene.addEventFilter(KeyEvent.KEY_RELEASED, KE -> {
+            if (scene.getRoot() != root) {
+                ft.stop();
+                double reachedOpacity = imgView.opacityProperty().doubleValue();
+                FadeTransition ft2 = new FadeTransition(Duration.millis(3000 * reachedOpacity), imgView);
+                ft2.setFromValue(reachedOpacity);
+                ft2.setToValue(0.0);
+                ft2.play();
+                ft2.setOnFinished(t -> {
+                    quitLoadingScreen();
+                });
+            }
+        });
+        stage.show();
+    }
+
+    private void quitLoadingScreen() {
+        scene.setRoot(root);
         scene.addEventFilter(KeyEvent.KEY_RELEASED, KE ->  {
             if (Objects.requireNonNull(KE.getCode()) == KeyCode.ESCAPE) {
                 System.exit(0);
@@ -72,40 +115,7 @@ public class App extends Application {
                     }
                 } catch (IOException ignored) {}
             }
-
         });
-        stage.setScene(scene);
-        //markdown example for fjuciur refernece
-        /*String mdfxTxt = IOUtils.toString(App.class.getResourceAsStream("sample.md"), "UTF-8");
-
-        MarkdownView markdownView = new MarkdownView(mdfxTxt) {
-            @Override
-            protected List<String> getDefaultStylesheets() {
-                return List.of(App.class.getResource("sample.css").toExternalForm());
-            }
-
-            @Override
-            public void setLink(Node node, String link, String description) {
-                System.out.println("setLink: " + link);
-                node.setCursor(Cursor.HAND);
-                node.setOnMouseClicked(e -> {
-                    System.out.println("link: " + link);
-                });
-            }
-        };
-        TextArea textArea = new TextArea(mdfxTxt);
-        markdownView.mdStringProperty().bind(textArea.textProperty());
-        markdownView.getStylesheets().add(App.class.getResource("sample.css").toExternalForm());
-        ScrollPane content = new ScrollPane();
-        content.setContent(markdownView);
-        content.setFitToWidth(true);
-        javafx.scene.layout.HBox.setHgrow(content, javafx.scene.layout.Priority.ALWAYS);
-        textArea.setMinWidth(350);
-        HBox root = new HBox(content);
-        Scene scene2 = new Scene(root, 700,700);
-        stage.setScene(scene2);*/
-        //markdown example
-        stage.show();
     }
 
     public static void setRoot(String fxml) throws IOException {
